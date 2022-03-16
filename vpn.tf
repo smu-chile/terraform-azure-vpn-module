01c762ae66c4e63205e266c08803dee0522097b8
Cristobal Diaz <cdiazm@smu.cl>
2022-03-16 07:52:31 -0300

    ADD VPN 10

--- vpn.tf
+++ vpn.tf
@@ -8,7 +8,7 @@ resource "azurerm_subnet" "gateway_subnet-a" {
   name                 = "GatewaySubnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = "cl-${var.app-name}-${var.environment}-${var.regions}"
-  address_prefixes       = cidrsubnet(data.azurerm_virtual_network.vnet-a.address_space[0], 10, 0)
+  address_prefix       = cidrsubnet(data.azurerm_virtual_network.vnet-a.address_space[0], 10, 0)
 }
 
 ## Creación de IP Pública Zona
